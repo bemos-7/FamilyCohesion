d5e328ccc8641259de30eab660b7769aaed202a3
bemos <bemo.0@mail.ru>
2025-02-11 21:33:41 +0300

    -> created new functions and data class, AppModule, DataModule

---
+++ app/src/main/java/com/bemos/familyohesion/domain/models/Categories.kt
@@ -0,0 +1,20 @@
+package com.bemos.familyohesion.domain.models
+
+data class Category(
+    val id: String = "",
+    val name: String = ""
+)
+
+data class Skill(
+    val id: String = "",
+    val name: String = "",
+    val categoryId: String = "",
+    val subSkills: List<SubSkill>? = null
+)
+
+data class SubSkill(
+    val id: String = "",
+    val name: String = "",
+    val points: Int = 0,
+    val skillId: String = ""
+)
