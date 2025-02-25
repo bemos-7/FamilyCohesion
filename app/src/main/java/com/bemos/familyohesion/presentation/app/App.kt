6933262bfa4b6ab069a2d3712323ca4b24f151ed
bemos <bemo.0@mail.ru>
2025-02-25 16:49:07 +0300

    - added list

--- app/src/main/java/com/bemos/familyohesion/presentation/app/App.kt
+++ app/src/main/java/com/bemos/familyohesion/presentation/app/App.kt
@@ -1,6 +1,8 @@
 package com.bemos.familyohesion.presentation.app
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateListOf
+import com.bemos.familyohesion.domain.models.SubSkill
 import com.bemos.familyohesion.presentation.di.app_component.AppComponent
 import com.bemos.familyohesion.presentation.di.app_component.DaggerAppComponent
 import com.google.firebase.Firebase
@@ -9,6 +11,10 @@ import com.google.firebase.initialize
 class App : Application() {
 
     lateinit var appComponent: AppComponent
+
+    companion object {
+        var listOfSubSkills = mutableStateListOf<SubSkill>()
+    }
 
     override fun onCreate() {
         super.onCreate()
