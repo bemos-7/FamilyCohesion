b41ef21a437af72a080bac173c69ae6555ce568a
bemos <bemo.0@mail.ru>
2025-02-25 16:47:55 +0300

    - bottomNavItems

---
+++ app/src/main/java/com/bemos/familyohesion/presentation/main_activity/util/BottomNavItem.kt
@@ -0,0 +1,29 @@
+package com.bemos.familyohesion.presentation.main_activity.util
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.bemos.familyohesion.R
+
+data class BottomNavItem(
+    val icon: Int,
+    val title: String,
+    val route: String
+)
+
+val bottomNavItems = listOf(
+    BottomNavItem(
+        icon = R.drawable.outline_school_24,
+        title = "Обучение",
+        route = "skills"
+    ),
+    BottomNavItem(
+        icon = R.drawable.hotel_class,
+        title = "Рейтинг",
+        route = "skills"
+    ),
+    BottomNavItem(
+        icon = R.drawable.profile,
+        title = "Профиль",
+        route = "profile"
+    ),
+)
