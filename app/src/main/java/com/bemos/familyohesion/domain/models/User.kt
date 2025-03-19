2ca5425bfec65bf6bff2acf9aecfa0216d45508f
bemos <bemo.0@mail.ru>
2025-03-19 20:32:10 +0300

    - added serializable

--- app/src/main/java/com/bemos/familyohesion/domain/models/User.kt
+++ app/src/main/java/com/bemos/familyohesion/domain/models/User.kt
@@ -1,5 +1,8 @@
 package com.bemos.familyohesion.domain.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class User(
     val email: String = "",
     val familyId: String = "",
