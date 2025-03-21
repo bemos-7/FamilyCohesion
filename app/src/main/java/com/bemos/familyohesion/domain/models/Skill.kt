0a3bac90a65a0c8a3a6ca56f197db07166c93f57
bemos <bemo.0@mail.ru>
2025-03-21 17:20:45 +0300

    - removed

--- app/src/main/java/com/bemos/familyohesion/domain/models/Skill.kt
+++
@@ -1,6 +0,0 @@
-package com.bemos.familyohesion.domain.models
-
-//data class Skill(
-//    val name: String,
-//    val subSkills: List<SubSkill>
-//)
--- app/src/main/java/com/bemos/familyohesion/domain/models/SubSkill.kt
+++
@@ -1,6 +0,0 @@
-package com.bemos.familyohesion.domain.models
-
-//data class SubSkill(
-//    val name: String,
-//    val points: Int
-//)
